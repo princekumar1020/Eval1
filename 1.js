02d05095d400cd2248536105bc7826eb3e217004
princekumar1020 <156185613+princekumar1020@users.noreply.github.com>
2024-10-23 22:12:59 +0530

    recharge javascript

---
+++ 1.js
@@ -0,0 +1,27 @@
+const net =document.getElementById("net");
+const Validity =document.getElementById("Validity");
+const output=document.getElementById("Amount");
+const pay=document.getElementById("payrupee");
+const checkamount=document.getElementById("checkamount");
+
+checkamount.addEventListener("click",
+    function (e){
+        e.preventDefault();
+        if(net.value==1){
+            output.innerHTML=(net.value*20*Validity.value);
+        }
+        else{
+            output.innerHTML=(net.value*35*Validity.value);
+        }
+    }
+)
+
+pay.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(output.textContent == 0){
+        alert("Anpad bhai data and days to fill kro");
+    }
+    else{
+        alert("You Transection was successfull !!!");
+    }
+});
