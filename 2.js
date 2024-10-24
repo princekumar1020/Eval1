9a4ef2ad880e44b67495b9b5286f3ef5dc7ec4e8
kirtansingla <kirtansingla@gmail.com>
2024-10-24 20:18:59 +0530

    mobile number format change

--- 2.js
+++ 2.js
@@ -2,7 +2,7 @@ document.getElementById('mobileForm').addEventListener('submit', function(event)
     event.preventDefault(); 
 
     const mobileNo = document.getElementById('mobileNo').value;
-    const regex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    const regex = /^[0-9]{3}[0-9]{3}[0-9]{4}$/;
 
     if (regex.test(mobileNo)) {
         window.location.href = 'prepaid.html';
